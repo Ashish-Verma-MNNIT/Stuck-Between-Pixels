4f16048277aa9896b1940349ce0da548a64f4975
Priyanshu Raj <priyanshuraj6649@gmail.com>
2023-03-25 00:28:24 +0530

    Navbar editing done

--- hack36/src/components/Navbar.jsx
+++ hack36/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function NavBar() {
     const [navbar, setNavbar] = useState(false);
 
     return (
-        <nav className="w-full shadow bg-slate-600">
+        <nav className="sticky top-0 z-50 w-full shadow bg-slate-800">
             <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
                 <div>
                     <div className="flex items-center justify-between py-3 md:py-5 md:block">
