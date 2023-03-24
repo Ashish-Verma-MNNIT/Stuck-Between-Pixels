e0b5d734f32ecf277af3db386ba5c659ff2943e8
Megha <megha13rai@gmail.com>
2023-03-25 00:12:17 +0530

    Adding Footer

--- hack36/src/pages/index.js
+++ hack36/src/pages/index.js
@@ -1,21 +1,20 @@
-import Head from "next/head"
-import Games from "@/components/Games"
+import Head from "next/head";
+import Games from "@/components/Games";
+import Footer from "../components/Footer";
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>My Games</title>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1"
-				/>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<main className="bg-gray-900">
-				<div className="">Choose from a list</div>
-				<Games />
-			</main>
-		</>
-	)
+  return (
+    <>
+      <Head>
+        <title>My Games</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-gray-900">
+        <div className="">Choose from a list</div>
+        <Games />
+      </main>
+      <Footer />
+    </>
+  );
 }
