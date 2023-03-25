0828d95a09ca181f82d9ddc958ede85d118b5c1a
NightHack36 <Sodharmendra74@gmail.com>
2023-03-25 20:34:48 +0530

    metamask

--- hack36/src/pages/_app.js
+++ hack36/src/pages/_app.js
@@ -1,15 +1,15 @@
 import Layout from "@/components/Layout"
 import "@/styles/globals.css"
-import MetaMask from './MetaMask'
+import MetaMask from '../components/MetaMask'
 
 
 
-export default function App({ Component, pageProps}) {
+export default function App({ Component, pageProps }) {
 	return (
-     
+
 		<Layout>
-	  {/* <MetaMask /> */}
-      <Component {...pageProps} />	
+			{/* <MetaMask /> */}
+			<Component {...pageProps} />
 		</Layout>
 	)
 }
--- hack36/src/pages/index.js
+++ hack36/src/pages/index.js
@@ -1,12 +1,8 @@
 import Head from "next/head"
-<<<<<<< HEAD
-import MetaMask from './MetaMask'
-=======
-import Login from "../pages/auth/login"
->>>>>>> 13534db20a67a947464c93c867afdb9e47232c56
+import MetaMask from '../components/MetaMask'
 
 export default function Home() {
-	
+
 	return (
 		<>
 			<Head>
@@ -20,9 +16,9 @@ export default function Home() {
 			<main className="py-1 text-white bg-gray-900">
 				<div className="text-2xl font-bold text-center"></div>
 				<div className="grid h-screen bg-blue-500 place-items-center">
-				<MetaMask/>
-				
-</div>
+					<MetaMask />
+
+				</div>
 			</main>
 		</>
 	)
