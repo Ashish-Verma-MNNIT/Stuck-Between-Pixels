1c789fb04375568b62a9929a25fc6b07a05d6f02
Ashish-Verma-MNNIT <ashishkv.2016@gmail.com>
2023-03-25 12:13:22 +0530

    login page

--- hack36/src/pages/index.js
+++ hack36/src/pages/index.js
@@ -1,11 +1,10 @@
 import Head from "next/head"
-import Games from "@/components/Games"
 
 export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>My Games</title>
+				<title>Homepage</title>
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1"
@@ -13,10 +12,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className="py-1 text-white bg-gray-900">
-				<div className="text-2xl font-bold text-center">
-					Choose from a list
-				</div>
-				<Games />
+				<div className="text-2xl font-bold text-center"></div>
 			</main>
 		</>
 	)
